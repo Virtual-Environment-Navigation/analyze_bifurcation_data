e91fc356bfb26f71de68546b66d72a968843b0a6
Trent Wirth <trentondwirth@gmail.com>
2023-10-04 09:56:27 -0400

    project initialized!
    there are some extraneous imports, but the `main.py` file now loads in our data as a pandas dataframe!

---
+++ config/config.py
@@ -0,0 +1,3 @@
+from pathlib import Path
+
+FINAL_HEADING_DATA = Path("D:\data_storage\\bifurcation_data\\2023_09_27_bifurcation_data_final_heading.csv")
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from config.config import FINAL_HEADING_DATA
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from scipy import stats
 from sklearn import mixture
+import pandas as pd
 
+df = pd.read_csv(FINAL_HEADING_DATA)
