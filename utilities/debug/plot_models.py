3b25b4d1633d3764139b021276a673b4f6146762
Trent Wirth <trentondwirth@gmail.com>
2023-10-18 09:58:39 -0400

    solved & cleaned
    model fitting is working, code base is cleaned up a bit
    
    next step: make decisions about how models are being fit...

--- utilities/debug/plot_models.py
+++ utilities/debug/plot_models.py
@@ -4,12 +4,10 @@ def plot_models(x_data, y_data, unimodal_model, bimodal_model):
 
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
 
-    #ax1.hist(y_data, bins=30, density=True, alpha=0.5, label='data')
     ax1.plot(x_data, y_data, label='unimodal model', marker='o')
     ax1.plot(x_data, unimodal_model, label='unimodal model', marker='x')
     ax1.legend()
 
-    #ax2.hist(y_data, bins=30, density=True, alpha=0.5, label='data')
     ax2.plot(x_data, y_data, label='bimodal model', marker='o')
     ax2.plot(x_data, bimodal_model, label='bimodal model', marker='x')
     ax2.legend()
--- utilities/model_fitting.py
+++ utilities/model_fitting.py
@@ -1,41 +1,28 @@
-
 import numpy as np
 from scipy.optimize import curve_fit
 from .models import unimodal, bimodal
 import matplotlib.pyplot as plt
+from utilities.HARD_CODED_VARIABLES import UNIMODAL_MEAN, HIST_BIN_NUMBER, UNIMODAL_AMPLITUDE, BIMODAL_AMPLITUDE_1, BIOMODAL_AMPLITUDE_2
 
 
-import numpy as np
-from scipy.optimize import curve_fit
-from .models import unimodal, bimodal
-import matplotlib.pyplot as plt
-
-from scipy.interpolate import make_interp_spline
-
-def model_fitting(data: list, mean, std_dev, alpha):
-
-    mean = 0 # reset input to == 0, if input is given at all. Reason for this is that 0 is a good prediction for that conditions heading
+def model_fitting(data: list, std_dev, alpha):
     
     x_data = np.linspace(min(data), max(data), num=len(data))
     y_data = np.array(data)
 
     # BEGIN: histogram
-    n, bins, patches = plt.hist(y_data, bins=30, density=True)
-    x_hist = bins
+    n, bins, _ = plt.hist(y_data, bins=HIST_BIN_NUMBER, density=True)
     y_hist = (bins[:-1] + bins[1:]) / 2
 
-    # Smooth out the curve
-    spl = make_interp_spline(y_hist, n)
-    y_smooth = spl(y_hist)
-
-    # Plot the smoothed curve on top of the histogram
-    plt.plot(y_hist, y_smooth, 'r')
-    plt.plot(y_hist, n)
+    plt.plot(y_hist, n, 'r')
     plt.show()
-    # END: histogram
 
-    params_uni, _ = curve_fit(lambda x, amplitude: unimodal(x, mean, std_dev, amplitude), y_data, x_data, p0=[0.1])
+    params_uni, _ = curve_fit(lambda x, amplitude: 
+                              unimodal(x, UNIMODAL_MEAN, std_dev, amplitude), 
+                              y_hist, n, p0=[UNIMODAL_AMPLITUDE])
     
-    params_bi, _ = curve_fit(lambda x, amplitude1, amplitude2: bimodal(x, alpha, std_dev, amplitude1, amplitude2), y_data, x_data, p0=[0.05, 0.05])
+    params_bi, _ = curve_fit(lambda x, amplitude1, amplitude2: 
+                             bimodal(x, alpha, std_dev, amplitude1, amplitude2), 
+                             y_hist, n, p0=[BIMODAL_AMPLITUDE_1, BIOMODAL_AMPLITUDE_2])
 
-    return x_data, y_data, params_uni, params_bi
+    return y_hist, n, params_uni, params_bi
